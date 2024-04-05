0ef608bf1597764a1f8d5a54a764f820d684d21c
Alex <alexkorman@gmail.com>
2024-04-05 16:14:37 -0700

    graph eval returns

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import sys
 from utils import save_agent, load_agent, plot_returns
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def episode(env, agent, nr_episode=0, evaluation_mode=False, verbose=True):
     """
@@ -60,17 +60,24 @@ def train(env, agent, episodes):
     """
     train_returns = []
     eval_returns = []
+    plotted_eval_returns = []
     for seed in range(no_seeds): # Loop through different random seeds
         # set random seeds
         np.random.seed(seed)
         random.seed(seed) 
          # Train on episodes and store returns
-        returns = [episode(env, agent, nr_episode=i, verbose=True) for i in range(episodes)]
+        for i in range (episodes):
+            if i % 10 == 0:
+                print(f"Episode {i}")
+                returns = evaluate(env, agent, runs=no_eval_runs, episodes=20)
+                plotted_eval_returns.append(np.mean(returns))
+            returns = episode(env, agent, nr_episode=i, verbose=False)
+            train_returns.append(returns)
         train_returns.append(returns)
         # Evaluate and store returns
-        returns = evaluate(env, agent, runs=no_runs, episodes=evaluation_episodes)
+        returns = evaluate(env, agent, runs=no_eval_runs, episodes=evaluation_episodes)
         eval_returns.append(returns)
-    return np.array(train_returns), np.array(eval_returns).reshape(no_seeds*no_runs, evaluation_episodes)
+    return np.array(train_returns), np.array(eval_returns).reshape(no_seeds*no_eval_runs, evaluation_episodes), np.array(plotted_eval_returns)
 
 # EVALUATION
 def evaluate(env, agent, runs, episodes):
@@ -87,7 +94,7 @@ def evaluate(env, agent, runs, episodes):
         eval_returns: Returns from evaluation episodes.
     """
     eval_returns = []
-    for _ in range(no_runs): 
+    for _ in range(no_eval_runs): 
         returns = [episode(env, agent, nr_episode=i, verbose=False, evaluation_mode=True) for i in range(episodes)]
         eval_returns.append(returns)
     return eval_returns
@@ -107,8 +114,8 @@ params["exploration_constant"] = np.sqrt(2)
 params["epsilon"] = 1
 training_episodes = 200
 evaluation_episodes = 20
-no_runs = 100
-no_seeds = 10
+no_eval_runs = 10
+no_seeds = 1
 
 # AGENTS
 #agent = a.RandomAgent(params)
@@ -119,18 +126,23 @@ agent = a.UCBQLearner(params)
 
 
 # TRAINING
-train_returns, eval_returns = train(env, agent, training_episodes)
+train_returns, eval_returns, plotted_eval_returns = train(env, agent, training_episodes)
+
 print(f"Trained for {training_episodes} episodes with {no_seeds} seeds.")
 print(f"Average training discounted return: {np.mean(train_returns)}")
-print(f"For each training seed, evaluated for {evaluation_episodes} episodes with {no_runs} runs.")
+print(f"For each training seed, evaluated for {evaluation_episodes} episodes with {no_eval_runs} runs.")
 print(f"Average evaluation discounted return: {np.mean(eval_returns)}")
-plot_returns(x=range(training_episodes), y=train_returns, evaluation_mode=False, instance=rooms_instance)
-plot_returns(x=range(evaluation_episodes), y=eval_returns, evaluation_mode=True, instance=rooms_instance)
+plt.plot(np.arange(len(plotted_eval_returns)), plotted_eval_returns)
+plt.show()
+
+# plot_returns(x=range(training_episodes), y=train_returns, evaluation_mode=False, instance=rooms_instance)
+print("y", plotted_eval_returns,eval_returns)
+# plot_returns(x=range(evaluation_episodes), y=eval_returns, evaluation_mode=True, instance=rooms_instance)
 # save_agent(agent)
 
 # EVALUATION
 # agent = load_agent("saved_agents/agent: 2024-03-19 13:01:51.pkl")
-# eval_returns = evaluate(env, agent, runs=no_runs, episodes=evaluation_episodes)
+# eval_returns = evaluate(env, agent, runs=no_eval_runs, episodes=evaluation_episodes)
 # plot_returns(x=range(evaluation_episodes), y=eval_returns)
 
 # env.save_video()
